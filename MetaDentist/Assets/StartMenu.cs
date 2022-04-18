2902211dab5ffd7daf060e6098c69cecdc549c19
ozcgokberk <ozcangokberk06@gmail.com>
2022-04-18 20:54:43 +0300

    Menu Added

---
+++ MetaDentist/Assets/StartMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+  public void PlayGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
